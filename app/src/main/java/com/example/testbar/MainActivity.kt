f8dbd2826950091942050eca6b68fc7837c42f82
Marlon Viana <marlonviana@gmail.com>
2019-11-10 17:37:13 -0400

    pintado programado lista de valores

--- app/src/main/java/com/example/testbar/MainActivity.kt
+++ app/src/main/java/com/example/testbar/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.testbar
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.util.TypedValue
+import android.view.Gravity
 import android.widget.TextView
 import com.github.guilhe.views.SeekBarRangedView
 import kotlinx.android.synthetic.main.activity_main.*
@@ -11,88 +14,103 @@ import android.widget.LinearLayout
 
 
 class MainActivity : AppCompatActivity() {
-
+    private var contadorCargas = 0
     private var listTextView: ArrayList<TextView> =ArrayList()
+
+    private var listaMaestraObtenida = false
+    private var listaMaestra:List<Float> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var listFloat= listOf(10F,22F,50F,85F,105F)
 
-        /** SEEKBAR **/
-        seekBar.setThumbNormalImageResource(R.drawable.ic_ucoin)
-        seekBar.setProgressStepRadius(10F)
-
-        seekBar.minValue = 0F
-        seekBar.maxValue = (listFloat.size-1).toFloat()
-
-        seekBar.enableProgressBySteps(true)
-        seekBar.progressSteps = itemStep(listFloat)
-
-        seekBar.setOnSeekBarRangedChangeListener(object :
-            SeekBarRangedView.OnSeekBarRangedChangeListener {
-            override fun onChanged(view: SeekBarRangedView, minValue: Float, maxValue: Float) {
-                updateLayout(minValue, maxValue)
-
-                paintText( getValuoFloat(minValue,listFloat),getValuoFloat(maxValue,listFloat),listFloat)
+        buttonChange.setOnClickListener{
+            if(contadorCargas==0){
+                var listFloat= listOf(5F,10F,22F,50F,85F,105F,111F,130F,200F)
+                setearLosDatos(listFloat)
+                contadorCargas++
+            }else if (contadorCargas==1){
+                var listFloat= listOf(5F,85F,200F)
+                setearLosDatos(listFloat)
+                contadorCargas++
+            }else if (contadorCargas == 2){
+                var listFloat= listOf(111F)
+                setearLosDatos(listFloat)
+                contadorCargas++
+            }else if (contadorCargas==3){
+                var listFloat= listOf(10F,50F,105F,130F)
+                setearLosDatos(listFloat)
+                contadorCargas++
+            }else{
+                contadorCargas = 0
             }
-
-            override fun onChanging(view: SeekBarRangedView, minValue: Float, maxValue: Float) {
-                updateLayout(minValue, maxValue)
-
-            }
-
-            private fun updateLayout(minValue: Float, maxValue: Float) {
-
-            }
-        })
-        //seekBar.setSelectedMinValue(20F, true)
-        /** GRIDLAYOUT **/
-        gridlayoutValues.columnCount = listFloat.size
-        for (i in listFloat.indices){
-            var titleText = TextView(applicationContext)
-            titleText.id = i
-            titleText.text = listFloat[i].toString()
+        }
 
 
-            var param = GridLayout.LayoutParams()
-            param.height = LinearLayout.LayoutParams.WRAP_CONTENT
-            param.width = LinearLayout.LayoutParams.WRAP_CONTENT
-            if (i != listFloat.size-1){
-                param.columnSpec = GridLayout.spec(GridLayout.UNDEFINED, GridLayout.FILL, 1f)
-            }
 
-            titleText.layoutParams = param
+    }
 
-            listTextView.add(titleText)
-            gridlayoutValues.addView(titleText,i)
+
+
+    private fun setearLosDatos(list:List<Float>){
+
+        if (!listaMaestraObtenida){
+            listaMaestra= list
+            iniciarPinatadoDeLista(listaMaestra)
+
+            listaMaestraObtenida= true
+        }else{
+
         }
 
     }
 
-    private fun itemStep(list:List<Float>):List<Float>{
-        var listParce: ArrayList<Float> = ArrayList()
 
-        for(i in 0 until list.size){
-            listParce.add(i.toFloat())
+    /** LISTA DE VALORES PINTADA **/
+
+    private fun iniciarPinatadoDeLista(list:List<Float>){
+
+        if (list.size>2){
+            gridlayoutValues.columnCount = list.size-2 //se le quitan dos colunan por los dos textview dentro de los linearlayout
         }
 
-        return listParce
-    }
+        for (i in list.indices) {
 
-    private fun getValuoFloat(itemLis:Float,list:List<Float>):Float{
-        return list[itemLis.toInt()]
-    }
+            if (i==0){
+                textFirtsValue.text = list[i].toInt().toString()
+                listTextView.add(textFirtsValue)
+            }else if (i == list.size-1){
+                textLastValue.text = list[i].toInt().toString()
+                listTextView.add(textLastValue)
+            }else{
+               var posicionColumana = i-1
 
-    @SuppressLint("ResourceAsColor", "NewApi")
-    private fun paintText(inital:Float, end:Float, list:List<Float>){
-        for (i in list.indices){
-          if (list[i]>= inital && list[i]<= end){
-              listTextView[i].setTextColor(getColor(R.color.colorPrimary))
-          }else{
-              listTextView[i].setTextColor(getColor(R.color.grayLight))
-          }
+                var titleText = TextView(this)
+                titleText.id = posicionColumana
+                titleText.text = list[i].toInt().toString()
+
+                var param = GridLayout.LayoutParams()
+                param.height = LinearLayout.LayoutParams.WRAP_CONTENT
+                param.width = LinearLayout.LayoutParams.WRAP_CONTENT
+                param.columnSpec =  GridLayout.spec(GridLayout.UNDEFINED, 1f)
+
+                titleText.gravity = Gravity.CENTER
+                titleText.layoutParams = param
+
+                listTextView.add(titleText)
+
+                gridlayoutValues.addView(titleText,posicionColumana)
+            }
         }
+
+        managerPintarLista()
+    }
+
+    private fun managerPintarLista(){
+        var numeroSteps = listaMaestra.size-1
+
+        Log.e("ANCHO TEX",textLastValue.width.toString())
+
     }
 }
